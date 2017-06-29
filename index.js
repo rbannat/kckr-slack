d667a74be098c2bd1e9090334118e743787552ff
Joachim Feldmann <joachim.feldmann@sevenval.com>
2017-06-29 23:54:18 +0200

    Simple date query

--- index.js
+++ index.js
@@ -2,9 +2,9 @@
 var express = require('express');
 var request = require('request');
 var bodyParser = require('body-parser');
+var moment = require('moment');
 
 require('dotenv').config();
-
 
 var clientId = process.env.CLIENT_ID;
 var clientSecret = process.env.CLIENT_SECRET;
@@ -12,7 +12,14 @@ var clientSecret = process.env.CLIENT_SECRET;
 var app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 
-const PORT=4390;
+var PORT=4390;
+
+var db = [{
+  id: 1,
+  time: '14:00',
+  createdBy: 'joa',
+  players: ['rene', 'stefan']
+}];
 
 app.listen(PORT, function () {
   //Callback triggered when server is successfully listening. Hurray!
@@ -24,16 +31,31 @@ app.get('/', function(req, res) {
 });
 
 app.post('/kickr/reserve', function(req, res) {
-  // res.json('Alles klar, um ' + req.body.text + ' Uhr ist frei');
-  res.json({
-    "response_type": "in_channel",
-    "text": "It's 80 degrees right now.",
-    "attachments": [
-        {
-            "text":"Partly cloudy today and tomorrow"
-        }
-    ]
-  }) ;
+
+  console.log(req.body.text);
+
+  // TODO Parse
+  var requiredMatchTime = moment(req.body.text, 'hh:mm');
+
+  const roomReserved = db.find(match => {
+
+    const matchStart = moment(match.time, 'hh:mm');
+    const matchEnd = moment(matchStart).add(20, 'minutes');
+    const isReserved = requiredMatchTime.isBetween(matchStart, matchEnd) || requiredMatchTime.isSame(matchStart);
+
+    return isReserved;
+  });
+
+  if (!roomReserved) {
+    res.json({
+      'response_type': 'in_channel',
+      'text': 'Raum ist frei und jetzt für Dich reserviert'
+    });
+  } else {
+    res.json({
+      'text': 'Sorry, Raum ist nicht frei ;('
+    });
+  }
 });
 
 app.post('/kickr/join/<12313>', function(req, res) {
