1a6db7cfb02887d480d609da2176d71d014ce67c
Stefan Schwartze <stefan.schwartze@sevenval.com>
2017-06-30 02:48:52 +0200

    added event emitters for controller

--- index.js
+++ index.js
@@ -61,109 +61,126 @@ server.listen(PORT, function () {
   console.log('Example app listening on port ' + PORT);
 });
 
-app.post('/kickr/reserve', function(req, res) {
+io.on('connection', function (socket) {
 
-  var requiredMatchTime = moment(req.body.text, 'HH:mm');
-  const match = isSlotFree(requiredMatchTime);
+  console.log('Uhh, connected!');
 
-  if (!match) {
 
-    db.push({
-      time: req.body.text,
-      createdBy: req.body.user_name,
-      players: [
-        req.body.user_name
-      ]
-    });
+  socket.on('request', function (data) {
+    console.log('Slot requested');
+    console.log(data);
+  });
+
+  app.post('/kickr/reserve', function(req, res) {
+
+    var requiredMatchTime = moment(req.body.text, 'HH:mm');
+    const match = isSlotFree(requiredMatchTime);
+
+    if (!match) {
+
+      db.push({
+        time: req.body.text,
+        createdBy: req.body.user_name,
+        players: [
+          req.body.user_name
+        ]
+      });
+
+      socket.emit('reserve_success', { data: 'reserved successful' });
+
+      res.json({
+        response_type: 'in_channel',
+        text: '<@' + req.body.user_id + '|' + req.body.user_name+ '> reserved a game at ' + req.body.text.split(' ')[0] + '! Wanna join?',
+        attachments: [{
+          text: 'Sure you wanna go down in hell?',
+          fallback: 'You are unable to choose a game',
+          callback_id: db.length - 1,
+          color: '#3AA3E3',
+          attachment_type: 'default',
+          actions: [{
+            name: 'yes',
+            text: "Yes ma'am!",
+            type: 'button',
+            value: 'yes',
+            style: 'primary',
+            response_url: process.env.HOST + '/kickr/join'
+          }]
+        }]
+      });
+    } else {
+      let text = 'Sorry, Raum ist nicht frei ;(';
+      text += match.createdBy + ' spielt von ';
+
+      socket.emit('reserve_fail', { data: 'Sorry, already in use' });
+
+      res.json({
+        text,
+        response_type: 'in_channel',
+        attachments: [
+          {
+            fallback: 'Upgrade your Slack client to use messages like these.',
+            color: '3AA3E3',
+            attachment_type: 'default',
+            callback_id: 'select_remote_1234',
+            actions: [
+              {
+                name: 'times_list',
+                text: 'Wähle eine andere Zeit!',
+                type: 'select',
+                options: getFreeSlots()
+              }
+            ],
+            response_url: process.env.HOST + '/kickr/reserve'
+          }
+        ]
+      });
+    }
+  });
+
+  app.post('/kickr/join', function(req, res) {
+
+    const json = JSON.parse(req.body.payload);
+    const matchId = json.callback_id;
+    const match = db[matchId];
+
+    if (!match) {
+      return res.json({
+        text: 'ID falsch'
+      })
+    }
+
+    if (match.createdBy === json.user.name) {
+      return res.json({
+        text: 'Du hast das Spiel erstellt, Idiot!',
+        replace_original: false,
+      })
+    }
+
+    if (match.players.find(player => player === json.user.name)) {
+      return res.json({
+        text: 'Doppelt zählt nicht!',
+        replace_original: false,
+      })
+    }
+
+
+    if (match.players.length === MAX_PLAYER) {
+      return res.json({
+        text: 'Sorry, schon ' + MAX_PLAYER + '!!',
+        replace_original: true,
+      })
+    }
+
+    match.players.push(json.user.name);
+
+    db[matchId] = match;
 
     res.json({
       response_type: 'in_channel',
-      text: '<@' + req.body.user_id + '|' + req.body.user_name+ '> reserved a game at ' + req.body.text.split(' ')[0] + '! Wanna join?',
-      attachments: [{
-        text: 'Sure you wanna go down in hell?',
-        fallback: 'You are unable to choose a game',
-        callback_id: db.length - 1,
-        color: '#3AA3E3',
-        attachment_type: 'default',
-        actions: [{
-          name: 'yes',
-          text: "Yes ma'am!",
-          type: 'button',
-          value: 'yes',
-          style: 'primary',
-          response_url: process.env.HOST + '/kickr/join'
-        }]
-      }]
+      replace_original: false,
+      text: '<@' + json.user.id + '|' + json.user.name + '> spielt mit ' + match.players.filter(name => name !== json.user.name).join(' ')
     });
-  } else {
-    let text = 'Sorry, Raum ist nicht frei ;(';
-    text += match.createdBy + ' spielt von ';
-
-    res.json({ 
-      text,
-      response_type: 'in_channel',
-      attachments: [
-        {
-          fallback: 'Upgrade your Slack client to use messages like these.',
-          color: '3AA3E3',
-          attachment_type: 'default',
-          callback_id: 'select_remote_1234',
-          actions: [
-            {
-              name: 'times_list',
-              text: 'Wähle eine andere Zeit!',
-              type: 'select',
-              options: getFreeSlots()
-            }
-          ],
-          response_url: process.env.HOST + '/kickr/reserve'
-        }
-      ]
-    }); 
-  }
-});
-
-app.post('/kickr/join', function(req, res) {
-
-  const json = JSON.parse(req.body.payload);
-  const matchId = json.callback_id;
-  const match = db[matchId];
-
-  if (!match) {
-    return res.json({
-      text: 'ID falsch'
-    })
-  }
-
-  if (match.createdBy === json.user.name) {
-    return res.json({
-      text: 'Du hast das Spiel erstellt, Idiot!',
-      replace_original: false,
-    })
-  }
-
-  if (match.players.find(player => player === json.user.name)) {
-    return res.json({
-      text: 'Doppelt zählt nicht!',
-      replace_original: false,
-    })
-  }
-
-
-  if (match.players.length === MAX_PLAYER) {
-    return res.json({
-      text: 'Sorry, schon ' + MAX_PLAYER + '!!',
-      replace_original: true,
-    })
-  }
-
-  match.players.push(json.user.name);
-
-  db[matchId] = match;
-
-  res.json({
-    response_type: 'in_channel',
-    replace_original: false,
-    text: '<@' + json.user.id + '|' + json.user.name + '> spielt mit ' + match.players.filter(name => name !== json.user.name).join(' ')
   });
+
 });
+
