ba047a513606b367e8bc25de7195bf90bc983157
René Bannat <rene.bannat@sevenval.com>
2017-06-30 10:34:35 +0200

    add action route

--- test.js
+++ test.js
@@ -30,7 +30,7 @@ function nearestFutureMinutes(interval, someMoment) {
 }
 
 function isSlotFree(requiredMatchTime) {
-  console.log(requiredMatchTime);
+  // console.log(requiredMatchTime);
   return db.find(match => {
     const matchStart = moment(match.time, 'HH:mm');
     const matchEnd = moment(matchStart).add(20, 'minutes');
@@ -61,30 +61,30 @@ server.listen(PORT, function () {
   console.log('Example app listening on port ' + PORT);
 });
 
-io.on('connection', (socket) => {
-  console.log('a user connected');
-});
-
 app.post('/kickr/reserve', function(req, res) {
 
   var requiredMatchTime = moment(req.body.text, 'HH:mm');
-  const match = isSlotFree(requiredMatchTime);
+  reserve(requiredMatchTime, req.body.user_id, req.body.user_name );
+
+});
+
+function reserve(timeString, userId, userName) {
+  const time = moment(timeString, 'HH:mm');
+  const match = isSlotFree(time);
 
   if (!match) {
 
     db.push({
-      time: req.body.text,
-      createdBy: req.body.user_name,
+      time: time,
+      createdBy: userName,
       players: [
-        req.body.user_name
+        userName
       ]
     });
 
-    io.emit('reserve_success', { data: 'reserved successful' });
-
     res.json({
       response_type: 'in_channel',
-      text: '<@' + req.body.user_id + '|' + req.body.user_name+ '> reserved a game at ' + req.body.text.split(' ')[0] + '! Wanna join?',
+      text: '<@' + userId + '|' + userName + '> reserved a game at ' + time.split(' ')[0] + '! Wanna join?',
       attachments: [{
         text: 'Sure you wanna go down in hell?',
         fallback: 'You are unable to choose a game',
@@ -95,22 +95,23 @@ app.post('/kickr/reserve', function(req, res) {
           name: 'yes',
           text: "Yes ma'am!",
           type: 'button',
-          value: 'yes',
-          style: 'primary',
-          response_url: process.env.HOST + '/kickr/join'
+          value: db.length - 1,
+          style: 'primary'
         }]
       }]
     });
   } else {
+    let text = 'Sorry, Raum ist nicht frei ;(';
+    text += match.createdBy + ' spielt von ';
+
     res.json({
-      text: 'Sorry, um ' + req.body.text + ' Uhr ist der Raum bereits von <@' + req.body.user_id + '|' + req.body.user_name+ '> belegt!',
-      response_type: 'in_channel',
+      text,
       attachments: [
         {
           fallback: 'Upgrade your Slack client to use messages like these.',
           color: '3AA3E3',
           attachment_type: 'default',
-          callback_id: 'select_times',
+          callback_id: 'select_time',
           actions: [
             {
               name: 'times_list',
@@ -119,22 +120,22 @@ app.post('/kickr/reserve', function(req, res) {
               options: getFreeSlots()
             }
           ],
-          response_url: process.env.HOST + '/kickr/reserve'
         }
       ]
     });
   }
-});
+}
 
 app.post('/kickr/action', function(req, res) {
 
   const json = JSON.parse(req.body.payload);
   const actionId = json.callback_id;
 
-  switch (actionId) {
+  switch(actionId) {
     case 'join_btn':
-      const matchId = db.length - 1;
-      const match   = db[matchId];
+
+      const matchId = json.callback_id;
+      const match = db[matchId];
 
       if (!match) {
         return res.json({
@@ -144,37 +145,38 @@ app.post('/kickr/action', function(req, res) {
 
       if (match.createdBy === json.user.name) {
         return res.json({
-          text            : 'Du hast das Spiel erstellt, Idiot!',
+          text: 'Du hast das Spiel erstellt, Idiot!',
           replace_original: false,
         })
       }
 
       if (match.players.find(player => player === json.user.name)) {
         return res.json({
-          text            : 'Doppelt zählt nicht!',
+          text: 'Doppelt zählt nicht!',
           replace_original: false,
         })
       }
 
-  if (match.players.length === MAX_PLAYER) {
-    return res.json({
-      text: 'Sorry, schon ' + MAX_PLAYER + '!!',
-      replace_original: true,
-    })
-  }
+      if (match.players.length === MAX_PLAYER) {
+        return res.json({
+          text: 'Sorry, schon ' + MAX_PLAYER + '!!',
+          replace_original: true,
+        })
+      }
 
       match.players.push(json.user.name);
 
       db[matchId] = match;
 
       res.json({
-        response_type   : 'in_channel',
+        response_type: 'in_channel',
         replace_original: false,
-        text            : '<@' + json.user.id + '|' + json.user.name + '> spielt mit ' + match.players.filter(name => name !== json.user.name).join(' ')
+        text: '<@' + json.user.id + '|' + json.user.name + '> spielt mit ' + match.players.filter(name => name !== json.user.name).join(' ')
       });
       break;
     case 'select_times':
-      reserve('14:00');
+      console.log(req.body);
+      reserve(json.actions.selected_options.value, json.user.id, json.user.name);
       break;
     default:
       break;
