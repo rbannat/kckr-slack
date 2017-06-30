d3d1299ec5b71b00884e47f8762fc1b8f6da9d79
Stefan Schwartze <stefan.schwartze@sevenval.com>
2017-06-30 17:49:45 +0200

    moved base URL

--- controller/index.js
+++ controller/index.js
@@ -1,8 +1,10 @@
 'use strict';
 
+const URL = 'https://563cf559.ngrok.io';
+
 // Import the interface to Tessel hardware
 const tessel = require('tessel');
-var socket   = require('socket.io-client')('https://dcd3dbfc.ngrok.io');
+var socket   = require('socket.io-client')(URL);
 
 var button  = tessel.port.A.pin[2];
 var blue    = tessel.port.A.pin[3];
@@ -11,9 +13,10 @@ var red     = tessel.port.A.pin[5];
 var request = require('request');
 
 // check running game
-request.get('https://dcd3dbfc.ngrok.io/kickr/free', function (error, response, body) {
+request.get(URL + '/kickr/free', function (error, response, body) {
 
   let options = JSON.parse(body);
+  console.log(options);
 
   if (options.runningMatch) {
     red.write(1);
@@ -31,14 +34,15 @@ button.on('change', (value) => {
 
   if (!btnPressed) {
     btnPressed = true;
-    request.post('https://dcd3dbfc.ngrok.io/kickr/reserve', function (error, response, body) {
+    request.post(URL + '/kickr/reserve', function (error, response, body) {
       console.log('error:', error); // Print the error if one occurred
       console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
       console.log('body:', body); // Print the body
 
-      let options = JSON.parse(body);
-      if (options.attachments[0].callback_id === 'match_actions') {
-        request.post('https://hooks.slack.com/services/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', options);
+      let options = body;
+      if (JSON.parse(options).attachments[0].callback_id === 'match_actions') {
+      	console.log(options);
+        request.post('https://hooks.slack.com/services/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', options);
         blue.write(1);
         red.write(0);
 
